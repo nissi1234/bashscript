54603b67750a8c4fc759f39ea7ab9037737ffb99
nishwanth raju <150512493+nissi1234@users.noreply.github.com>
2024-03-19 16:09:23 +0530

    Update nishwanth.sh

--- nishwanth.sh
+++ nishwanth.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum -y install git
 git clone https://github.com/nissi1234/car-prediction.git
-cd car-prediction.git
+cd car-prediction
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
